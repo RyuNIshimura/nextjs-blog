89dfec280bcb3a5b467c449119e8bdbb474b6348
Ryu Nishimura <40055484+RyuNIshimura@users.noreply.github.com>
2021-10-09 22:29:47 +0900

    refactor

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -2,23 +2,20 @@
 import { TwitterIcon, GitHubIcon } from '@/components/svg';
 import { Sns } from '@/@types/index';
 
-export const APP_NAME: string = 'Nishimura Club ⚡️';
-export const BASE_URL: string = 'https://nishimura.club';
-export const META_DESCRIPTION: string =
-  'JavaScriptとWeb開発のチュートリアル ⚡️';
-
-export const PER_PAGE: number = 15;
-export const PER_SEARCH: number = 5;
-export const RELATED_ARTICLES_LIMIT: number = 9;
+export const APP_NAME = 'Nishimura Club ⚡️';
+export const BASE_URL = 'https://nishimura.club';
+export const META_DESCRIPTION = 'JavaScriptとWeb開発のチュートリアル ⚡️';
+export const TWITTER_ID = 'ryunishimura87';
+export const GITHUB_ID = 'RyuNIshimura';
+export const PER_PAGE = 15;
+export const PER_SEARCH = 5;
+export const RELATED_ARTICLES_LIMIT = 9;
 
 export enum CONTENT_TYPE {
   ARTICLE = 'article',
   CATEGORY = 'types',
   TAG = 'tags',
 }
-
-export const TWITTER_ID: string = 'ryunishimura87';
-export const GITHUB_ID: string = 'RyuNIshimura';
 
 export const SNS_ITEMS: Sns[] = [
   {
--- lib/markdown-utils.ts
+++ lib/markdown-utils.ts
@@ -1,7 +1,7 @@
 import remark from 'remark';
 import html from 'remark-html';
 
-export const copyText = ({ text }: { text: string }) => {
+export const copyText = ({ text = '' }: { text: string }) => {
   navigator.clipboard.writeText(text);
 };
 
@@ -11,17 +11,17 @@ export const getExtend = ({ filename = '' }: { filename: string }) => {
   return filename.slice(pos + 1);
 };
 
-export const removeHtmlTags = ({ htmlString }: { htmlString: string }) => {
+export const removeHtmlTags = ({ htmlString = '' }: { htmlString: string }) => {
   const htmlTagPattern = new RegExp('<("[^"]*"|\'[^\']*\'|[^\'">])*>', 'g');
   return htmlString.replace(htmlTagPattern, '');
 };
 
-export const removeNewLine = ({ text }: { text: string }) => {
+export const removeNewLine = ({ text = '' }: { text: string }) => {
   const newText = text.replace(/\r?\n/g, '');
   return newText;
 };
 
-export const getMetaDescription = async ({ text }: { text: string }) => {
+export const getMetaDescription = async ({ text = '' }: { text: string }) => {
   const result = await remark()?.use(html)?.process(text);
   const htmlString = removeNewLine({ text: result.toString() });
   const description = removeHtmlTags({ htmlString }).slice(0, 120) + '...';
